ea7fc7ecafae3b7f8a6495f9b7aaaba7d692508f
weronedatabase-tech <werone.database@gmail.com>
2026-04-20 06:44:45 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'myg-trip-v1';
+const urlsToCache =[
+  './',
+  './index.html',
+  './styles.css',
+  './tailwind.config.js',
+  './app.js',
+  './auth.js',
+  './ui.js',
+  './rolodex.js',
+  './registration.js',
+  './profile.js',
+  './logistics.js',
+  './attendance.js',
+  './settings.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request).catch(() => caches.match('./index.html'));
+    })
+  );
+});
