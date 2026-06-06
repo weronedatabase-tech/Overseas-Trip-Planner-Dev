310b21de7d65129a6c5814e5f1e0d2a6d682601c
weronedatabase-tech <werone.database@gmail.com>
2026-06-06 15:07:56 +0800

    Automated emergency update via App Code Maintainer

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'myg-trip-v17';
+const CACHE_NAME = 'myg-trip-v18';
 const urlsToCache =[
 './',
 './index.html',
@@ -28,13 +28,13 @@ self.addEventListener('activate', event => {
 // Delete old caches (like v1, v2, v3) so the new folder structure and config takes over
 event.waitUntil(
 caches.keys().then(cacheNames => {
- return Promise.all(
-   cacheNames.map(cacheName => {
-     if (cacheName !== CACHE_NAME) {
-       return caches.delete(cacheName);
-     }
-   })
- );
+return Promise.all(
+  cacheNames.map(cacheName => {
+    if (cacheName !== CACHE_NAME) {
+      return caches.delete(cacheName);
+    }
+  })
+);
 }).then(() => self.clients.claim()) // Instantly take control of uncontrolled clients
 );
 });
@@ -42,7 +42,7 @@ caches.keys().then(cacheNames => {
 self.addEventListener('fetch', event => {
 event.respondWith(
 caches.match(event.request).then(response => {
- return response || fetch(event.request).catch(() => caches.match('./index.html'));
+return response || fetch(event.request).catch(() => caches.match('./index.html'));
 })
 );
 });
