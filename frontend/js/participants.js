66f61b95aa1767fe1156607fda4067f1f47c8690
weronedatabase-tech <werone.database@gmail.com>
2026-08-06 21:47:42 +0100

    Emergency Repository Rollback via App Code Maintainer (MYG)

--- frontend/js/participants.js
+++ frontend/js/participants.js
@@ -300,10 +300,6 @@ document.removeEventListener('touchend', onMouseUp);
 
 let draggedColId = null;
 window.onColDragStart = function(e, colId) {
-if (resizingCol) {
-    e.preventDefault();
-    return;
-}
 draggedColId = colId;
 e.dataTransfer.effectAllowed = "move";
 e.target.classList.add('opacity-50');
